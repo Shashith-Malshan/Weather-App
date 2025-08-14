2159063bbcacaf6a529b044cd6a439b1748ff729
Shashith-Malshan <malshanshashith318@gmail.com>
2025-08-14 14:59:01 +0530

    Add local time and date

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,3 +2,23 @@ console.log("JS Loaded");
 
 AOS.init();
 
+let date = new Date();
+console.log(date);
+
+formatDate = {
+    "weekday":`long`,
+    "month":`long`,
+    "year" : `numeric`,
+    "day" : `numeric`
+}
+
+let currentDate = date.toLocaleDateString("en-US",formatDate);
+// document.getElementById("date").innerText = currentDate;
+console.log(currentDate);
+
+let currentTime=date.toLocaleTimeString();
+console.log(currentTime);
+
+document.getElementById("local-date").innerText = currentDate;
+document.getElementById("local-time").innerText = currentTime;
+
